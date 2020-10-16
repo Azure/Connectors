d2fe85deab0c53ba4448bbc9bfb7b3304fea08df
Jack <japa@microsoft.com>
2020-10-16 10:29:18 -0700

    remove unneeded flag passed into autorest

--- sdk/CSharp/Scripts/RunAutoRestDotNet.ps1
+++ sdk/CSharp/Scripts/RunAutoRestDotNet.ps1
@@ -22,7 +22,7 @@ if(Test-Path ./sdk/scripts/SwaggerTransform/$($connectorName).ps1)
 $TextInfo = (Get-Culture).TextInfo
 $capitalizedName = if (($friendlyName -eq $null) -or ($friendlyName -eq "")) { $TextInfo.ToTitleCase($connectorName) } else { $friendlyName }
 if ($isLocal) {
-    autorest ./$($config) --input-file:./sdk/swaggers/ms-services/released/$($connectorName).json --output-folder:./ScriptGenerated/$($connectorName) --override-client-name:$($capitalizedName)Connector --namespace:Azure.Connectors.$($capitalizedName) --csharp-only --$($connectorName)
+    autorest ./$($config) --input-file:./sdk/swaggers/ms-services/released/$($connectorName).json --output-folder:./ScriptGenerated/$($connectorName) --override-client-name:$($capitalizedName)Connector --namespace:Azure.Connectors.$($capitalizedName) --csharp-only
 } else {
-    autorest ./$($config) --input-file:./RetrievedSwaggers/$($connectorName).json --output-folder:./ScriptGenerated/$($connectorName) --override-client-name:$($capitalizedName)Connector --namespace:Azure.Connectors.$($capitalizedName) --csharp-only --$($connectorName)
+    autorest ./$($config) --input-file:./RetrievedSwaggers/$($connectorName).json --output-folder:./ScriptGenerated/$($connectorName) --override-client-name:$($capitalizedName)Connector --namespace:Azure.Connectors.$($capitalizedName) --csharp-only
 }
