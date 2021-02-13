cd701e8278bf9871b2752f6e526a688cfb8afa82
Sean Kim <seaki@microsoft.com>
2021-02-12 18:29:34 -0800

    fix RunAutorest and compile script for typescript

--- sdk/Typescript/Scripts/CompileTypescript.ps1
+++ sdk/Typescript/Scripts/CompileTypescript.ps1
@@ -3,38 +3,49 @@ Param(
     [string] [Parameter(Mandatory=$true)] [AllowEmptyString()] $friendlyName
 )
 
+$root = Get-Location
+Set-Location ScriptGenerated/$($connectorName)/typescript
+
 # Install azure identity npm
 Write-Output "importing azure identity"
-$IdentityCommand = "npm i @azure/identity --prefix ./ScriptGenerated/$($connectorName)/typescript --save-bundle"
+$IdentityCommand = "npm i @azure/identity --save-bundle"
 Invoke-Expression $IdentityCommand
 # Install ms-rest-js library as bundledDependencies
 Write-Output "saving azure ms-rest-js as bundle"
-$BundleMsRestCommand = "npm i @azure/ms-rest-js --prefix ./ScriptGenerated/$($connectorName)/typescript --save-bundle"
+$BundleMsRestCommand = "npm i @azure/ms-rest-js --save-bundle"
 Invoke-Expression $BundleMsRestCommand
 # Install ms-rest-azure-js library as bundledDependencies
 Write-Output "saving azure ms-rest-azure-js as bundle"
-$BundleAzureMsRestCommand = "npm i @azure/ms-rest-azure-js --prefix ./ScriptGenerated/$($connectorName)/typescript --save-bundle"
+$BundleAzureMsRestCommand = "npm i @azure/ms-rest-azure-js --save-bundle"
 Invoke-Expression $BundleAzureMsRestCommand
 
 # Install 
-$rollupConfigFile = "./ScriptGenerated/$($connectorName)/typescript/rollup.config.js"
-(Get-Content $rollupConfigFile ).Replace('"@azure/ms-rest-js",','"@azure/ms-rest-js", "@azure/identity",') | Out-File $rollupConfigFile
+$rollupConfigFile = "./rollup.config.js"
+(Get-Content $rollupConfigFile ).Replace('"@azure/ms-rest-js",', '"@azure/ms-rest-js", "@azure/identity",') | Out-File $rollupConfigFile -Encoding "UTF8"
 
 # Fix package.json
-$packageJsonFile = "./ScriptGenerated/$($connectorName)/typescript/package.json"
-(Get-Content $packageJsonFile).Replace('https://github.com/Azure/azure-sdk-for-js','ssh://git@github.com/Azure/Connectors') | Out-File $packageJsonFile
+$packageJsonFile = "./package.json"
+$packageJson = (Get-Content $packageJsonFile)
+(Get-Content $packageJsonFile).Replace('https://github.com/Azure/azure-sdk-for-js', 'ssh://git@github.com/Azure/Connectors') | Out-File $packageJsonFile -Encoding "UTF8"
+$version = ($packageJson | ConvertFrom-Json).version
+
 # Fix README.md
 $TextInfo = (Get-Culture).TextInfo
 $capitalizedName = if (($friendlyName -eq $null) -or ($friendlyName -eq "")) { $TextInfo.ToTitleCase($connectorName) } else { $friendlyName }
-$readmeFile = "./ScriptGenerated/$($connectorName)/typescript/README.md"
-Copy-Item ./sdk/Typescript/readme.md -Destination $readmeFile -Force
-(Get-Content $readmeFile).Replace('<connector-name>', $capitalizedName) | Out-File $readmeFile
-(Get-Content $readmeFile).Replace('<connector-name-lower>', $capitalizedName.ToLower()) | Out-File $readmeFile
-(Get-Content $readmeFile).Replace('<connector-config-name>', $connectorName) | Out-File $readmeFile
+$readmeFile = "./README.md"
+Copy-Item $root/sdk/Typescript/readme.md -Destination $readmeFile -Force
+(Get-Content $readmeFile).Replace('<connector-name>', $capitalizedName) | Out-File $readmeFile -Encoding "UTF8"
+(Get-Content $readmeFile).Replace('<connector-name-lower>', $capitalizedName.ToLower()) | Out-File $readmeFile -Encoding "UTF8"
+(Get-Content $readmeFile).Replace('<connector-config-name>', $connectorName) | Out-File $readmeFile -Encoding "UTF8"
 
 # install and publish npm package
 Write-Output "running npm install on $($connectorName)"
-npm install ScriptGenerated/$($connectorName)/typescript
+npm install 
 
-#Write-Output "running npm pack on $($connectorName)"
-npm pack ScriptGenerated/$($connectorName)/typescript
+Write-Output "running npm pack on $($connectorName)"
+npm pack
+
+$zipFilename = "azure-$($capitalizedName.ToLower())-connector-$($version).tgz"
+Copy-Item "./$($zipFilename)" -Destination "$($root)/$($zipFilename)"
+
+Set-Location $root
