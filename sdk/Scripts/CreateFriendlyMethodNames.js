76cc978dc0f7094a8a36afd2074a8a5201a7140b
Sean Kim <seaki@microsoft.com>
2021-03-14 23:56:24 -0700

    refactored the current CreateFriendlyMethodNames.js and checked that it works on swagger creation

--- sdk/Scripts/CreateFriendlyMethodNames.js
+++ sdk/Scripts/CreateFriendlyMethodNames.js
@@ -1,21 +1,46 @@
 // This script is a helper to quickly create friendly swagger names
 // Ignore trigger and deprecated API's
 //
-// First argument: File path to swagger
-// Second argument (optional): File path to output swagger. Will overwrite if none.
-//
+// First argument: change mode.
+//   - 'config': create a custom autorest config file with rename-operation
+//   - 'swagger': create/update a swagger file with 'x-ms-client-name' field
+// Second argument: File path to swagger
+// Third argument (optional): File path to the output autorest config or swagger. 
+// Use default value if not specified, which is
+//   - 'config': sdk/autorest/customConfigs/${friendlyName}
+//   - 'swagger': {same as second argument (overwrite the existing one)}
+
 // example:
-// > node sdk/Scripts/CreateFriendlyMethodNames.js sdk/swaggers/ms-services/released/aci.json aci2.json
+// > node sdk/Scripts/CreateFriendlyMethodNames.js config sdk/swaggers/ms-services/released/aci.json sdk\autorest\customConfigs\aci.md
+// > node sdk/Scripts/CreateFriendlyMethodNames.js swagger sdk/swaggers/ms-services/released/aci.json aci2.json
 
 const readline = require('readline');
 const util = require('util');
 const fs = require('fs');
+const { exit } = require('process');
 
 const JSON_SPACING = "  ";
 const myArgs = process.argv.slice(2);
-const swaggerFile = myArgs[0];
-const outputSwaggerFile = myArgs[1];
+const changeMode = myArgs[0];
+const swaggerFile = myArgs[1];
+const outputFile = myArgs[2];
 const readFileAsync = util.promisify(fs.readFile);
+
+const customConfig = "\
+# {connectorName\} Custom Config\
+\
+> see https://aka.ms/autorest\
+\
+## Configuration\
+\
+```yaml\
+# https://github.com/Azure/autorest/blob/master/Samples/openapi-v2/3h-try-require/readme.md\
+require: ../readme.md\
+\
+# Add your own config below\
+{directives\}\
+```\
+"
 
 function askQuestion(query) {
     const rl = readline.createInterface({
@@ -50,21 +75,10 @@ async function continueOrTerminate() {
     }
 }
 
-async function run() {
+async function getNewNamesFromUser(swagger) {
     try {
-        // Prompts to make sure the user knows what they're doing
-        if (outputSwaggerFile) {
-            console.log(`Reading swagger from: '${swaggerFile}'`)
-            console.log(`Writing swagger to: '${outputSwaggerFile}'`)
-            await continueOrTerminate();
-        } else {
-            console.warn(`IMPORTANT: this script will overwrite the existing contents of ${swaggerFile}`);
-            await continueOrTerminate();
-        }
         // Loop through API's and optionally rename operationId's
-        const changes = {};
-        const swaggerData = await readFileAsync(swaggerFile, 'utf8');
-        const swagger = JSON.parse(swaggerData);
+        const newNames = {};
         for (let path in swagger.paths) {
             for (let method in swagger.paths[path]) {
                 let action = swagger.paths[path][method];
@@ -80,25 +94,71 @@ async function run() {
                 console.log(`x-ms-client-name: \t${action["x-ms-client-name"]}`);
                 let newName = await getName();
                 if (newName) {
-                    changes[action.operationId] = newName;
-                    action["x-ms-client-name"] = newName;
+                    newNames[action.operationId] = newName;
                 }
             }
         }
 
         // Confirm one last time with the user that everything looks good
         console.warn("\nMaking the following changes:")
-        for (let oldName in changes) {
-            console.log(`\t${oldName} => ${changes[oldName]}`);
+        for (let oldName in newNames) {
+            console.log(`\t${oldName} => ${newNames[oldName]}`);
         }
         await continueOrTerminate();
-
-        const output = outputSwaggerFile || swaggerFilel
-        fs.writeFileSync(output, JSON.stringify(swagger, null, JSON_SPACING));
+        return newNames;
     } catch (err) {
         console.error(err)
     }
 }
 
-run();
+async function createNewSwagger(swagger, newNames, outputSwaggerFile) {
+    for (let path in swagger.paths) {
+        for (let method in swagger.paths[path]) {
+            let action = swagger.paths[path][method];
+            if (action["x-ms-trigger"] || action.deprecated) {
+                continue;
+            }
+            if (action.operationId in newNames) {
+                action["x-ms-client-name"] = newNames[action.operationId];
+            }
+        }
+    }
 
+    fs.writeFileSync(outputSwaggerFile, JSON.stringify(swagger, null, JSON_SPACING));
+}
+
+async function createNewCustomConfig(swagger, newNames, outputFile) {
+    // TODO(sean)
+}
+
+async function run() {
+    // Verify change mode input
+    if (changeMode != "config" && changeMode != "swagger") {
+        console.error(`changeMode has to be either 'config' or 'swagger': current=${changeMode}`);
+        exit(1);
+    } 
+
+    console.log(`Reading swagger from: '${swaggerFile}'`)
+    const swaggerData = await readFileAsync(swaggerFile, 'utf8');
+    const swagger = JSON.parse(swaggerData);
+
+    if (changeMode == "config") {
+        const newNames = await getNewNamesFromUser(swagger);
+        createNewCustomConfig(swagger, newNames, outputFile)
+    }
+    if (changeMode == "swagger") {
+        // Prompts to make sure the user knows what they're doing
+        if (outputFile) {
+            console.log(`Writing swagger to: '${outputFile}'`)
+            await continueOrTerminate();
+        } else {
+            console.warn(`IMPORTANT: this script will overwrite the existing contents of ${swaggerFile}`);
+            await continueOrTerminate();
+        }
+
+        const newNames = await getNewNamesFromUser(swagger);
+        createNewSwagger(swagger, newNames, outputFile || swaggerFile)
+    }
+}
+
+run();
